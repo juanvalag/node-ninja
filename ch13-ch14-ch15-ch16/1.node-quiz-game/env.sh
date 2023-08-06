1009b988995a81407fb34e4614b248f53e045e78
Juan Valencia <Juan.Valencia@endava.com>
2023-08-05 22:37:02 -0500

    setting env

--- ch13-ch14-ch15-ch16/1.node-quiz-game/env.sh
+++ ch13-ch14-ch15-ch16/1.node-quiz-game/env.sh
@@ -10,7 +10,7 @@ export POSTGRES_QUIZUSER=quizuser
 export POSTGRES_QUIZPASS=quizpass
 
 # application settings
-export QUIZ_TITLE=Multiplayer Quiz
+export QUIZ_TITLE="Multiplayer Quiz"
 export QUIZ_WEB_DOMAIN=http://quiz.localhost
 export QUIZ_WS_DOMAIN=ws://quizws.localhost/
 export QUIZ_QUESTIONS_MAX=500
