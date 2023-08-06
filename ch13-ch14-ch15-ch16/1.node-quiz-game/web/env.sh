1c52d0fd3c12733f6e798d73acebbe370940e913
Juan Valencia <Juan.Valencia@endava.com>
2023-08-05 20:51:40 -0500

    loading env

---
+++ ch13-ch14-ch15-ch16/1.node-quiz-game/web/env.sh
@@ -0,0 +1,16 @@
+# database credentials (root)
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=pgpassword
+
+# quiz user credentials (app)
+export POSTGRES_SERVER=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_DB=quiz
+export POSTGRES_QUIZUSER=quizuser
+export POSTGRES_QUIZPASS=quizpass
+
+# application settings
+export QUIZ_TITLE=Multiplayer Quiz
+export QUIZ_WEB_DOMAIN=http://quiz.localhost
+export QUIZ_WS_DOMAIN=ws://quizws.localhost/
+export QUIZ_QUESTIONS_MAX=500
